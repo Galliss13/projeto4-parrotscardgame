1cd9c7a09cdf47ff8c06583b80e328272dbb98be
Matheus Galindo <matheusgalindo1313@gmail.com>
2022-08-21 21:43:46 -0300

    animação de virar carta completa

--- parrot.js
+++ parrot.js
@@ -30,6 +30,13 @@ const cartinhas_diferentes = [
     'parrot_imagens/roofparrot.gif',
     'parrot_imagens/michaeljacksonparrot.gif'
 ]
+const contador = 0;
+let armazena_sorteio = []
+let ListaCartas = [];
+let ParesOrdenados = [];
+let numero_cartas = 0;
+let verificador = 0;
+let par = false
 
 function comparador() {
     return Math.random() - 0.5;
@@ -37,24 +44,54 @@ function comparador() {
 
 function start() {
     //prompt inicial
-    let numero_cartas = prompt ("Com quantas cartas você quer jogar?");
+    numero_cartas = prompt ("Com quantas cartas você quer jogar?");
     while (numero_cartas % 2 !== 0 || numero_cartas < 4){
         numero_cartas = prompt ("Escreva um número par maior que 3. Com quantas cartas você quer jogar?");
     }
-    //disposição inicial das cartas
-    for(let cont=0; cont < numero_cartas; cont++) {
-        let campo = document.querySelector('.campocartas');
-        campo.innerHTML = campo.innerHTML + '<div class="carta"><img src="parrot_imagens/front.png"></div>'
-    }
-    //sorteio das diferentes cartas
+
+    //sorteio das diferentes cartas, colocando-as em pares numa nova lista embaralhada
     let embaralhamento = cartinhas_diferentes.sort(comparador);
     const sorteio = [];
     for(let cont=0; cont < numero_cartas/2; cont++) {
-        sorteio.push(embaralhamento[cont])
+        sorteio.push(embaralhamento[cont]);
+        sorteio.push(embaralhamento[cont]);
     }
-    console.log(sorteio);
-    console.log(sorteio.length);
+    let sorteio_embaralhado = sorteio.sort(comparador);
+    for(let k=0; k <numero_cartas; k++) {
+        armazena_sorteio.push(sorteio_embaralhado[k])
+    }
+    
+   //disposição inicial das cartas
+    for(let cont=0; cont < numero_cartas; cont++) {
+        let campo = document.querySelector('.campocartas');
+        ListaCartas.push( `<div onclick="cartaClicada(this)" class="carta n${cont}">
+        <img src="parrot_imagens/front.png">
+        <img class="hidden" src="${sorteio_embaralhado[cont]}">
+        </div>`)
+        campo.innerHTML = campo.innerHTML + ListaCartas[cont];
+    }
+    console.log(armazena_sorteio)
+
+    //associação entre os pares distribuídos aleatoriamente
+    for(let i=0; i <numero_cartas; i++) {
+        for(let j=0; j <numero_cartas; j++) {
+            if (i !== j) {
+                if (sorteio_embaralhado[i] === sorteio_embaralhado[j]){
+                    ParesOrdenados.push([`n${i}`,`n${j}`]);
+                }
+            }
+        }
+    }
+    console.log(ParesOrdenados);
 }
+
+
 start()
 
+function cartaClicada(elemento) {
+    const childimg =  elemento.childNodes;
+    childimg[1].classList.add('hidden')
+    childimg[3].classList.remove('hidden')
+    elemento.classList.add('efeito')
+}
 
